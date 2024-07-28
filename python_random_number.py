a9b7d6bd7cf4109fb7e1ec6490ce0c15e4e46425
soremi kayode <banabaz.sk@gmail.com>
2024-07-28 21:37:07 +0100

    auto

--- python_random_number.py
+++ python_random_number.py
@@ -1,3 +1,10 @@
 import random
-
+""" 
+, '_warn', 'betavariate', 'choice', 'choices', 'expovariate',
+'gammavariate', 'gauss', 'getrandbits', 'getstate', 'lognormvariate',
+'normalvariate', 'paretovariate', 'randbytes', 'randint', 'random',
+'randrange', 'sample', 'seed', 'setstate', 'shuffle', 'triangular',
+'uniform', 'vonmisesvariate', 'weibullvariate'
+"""
+#.random generate random number between zero and one
 print(random.random()) # generate random number between number
